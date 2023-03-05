5577b71e17c55ac7f7b5d39ad07547fa90323e65
most_love08 <murakoudai@gmail.com>
2023-03-05 11:48:53 +0900

    add func2

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          FuncA1 started by editing{" "}
+          FuncA2 started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
